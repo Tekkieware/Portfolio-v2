6a775ca8154151be1160c2bff2476c5e8284abeb
Isaiah Ozadhe <tekkieware@gmail.com>
2025-05-18 14:07:54 +0100

    added upload to cloudinary logic

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,37 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+
+export const uploadFileToCloudinary = async (
+  file: File,
+  path: string
+): Promise<string> => {
+  const url = "https://api.cloudinary.com/v1_1/dcajjwtba/image/upload";
+  const uploadPreset = "portfolio";
+  const folder = `portfolio/${path}`;
+  
+  try {
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("folder", folder);
+    formData.append("upload_preset", uploadPreset);
+
+    const response = await fetch(url, {
+      method: "POST",
+      body: formData,
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to upload file");
+    }
+
+    const data: { secure_url: string } = await response.json();
+    return data.secure_url;
+  } catch (error) {
+    console.error("Error uploading file:", error);
+    throw error;
+  }
+};
+
